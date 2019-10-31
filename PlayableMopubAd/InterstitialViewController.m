dca81071ae847d5688b7fecbe1c1a5c23841e0d6
ShunZhi Tang <tangshunzhi@zplay.com>
2019-10-31 15:59:30 +0800

    Replace new test placementID

--- PlayableMopubAd/InterstitialViewController.m
+++ PlayableMopubAd/InterstitialViewController.m
@@ -23,7 +23,7 @@
 - (void)loadInterstitial {
     // Instantiate the interstitial using the class convenience method.
     self.interstitial = [MPInterstitialAdController
-                         interstitialAdControllerForAdUnitId:@"d5c5566b8e144a3cae4cedd45f299696"];
+                         interstitialAdControllerForAdUnitId:@"5d50f8affae04961a88834e5fe9c6dc6"];
     self.interstitial.delegate = self;
     
     // Fetch the interstitial ad.
