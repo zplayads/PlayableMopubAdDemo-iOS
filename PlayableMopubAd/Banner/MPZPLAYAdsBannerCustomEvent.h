6ee7b91f444089db9cecad432bc65d580d530dee
ShunZhi Tang <tangshunzhi@zplay.com>
2019-10-31 15:56:15 +0800

    Add zplay banner custom event

---
+++ PlayableMopubAd/Banner/MPZPLAYAdsBannerCustomEvent.h
@@ -0,0 +1,18 @@
+//
+//  MPZPLAYAdsBannerCustomEvent.h
+//  PlayableMopubAd
+//
+//  Created by Michael Tang on 2019/10/31.
+//  Copyright © 2019 lgd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MPBannerCustomEvent.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPZPLAYAdsBannerCustomEvent : MPBannerCustomEvent
+
+@end
+
+NS_ASSUME_NONNULL_END
